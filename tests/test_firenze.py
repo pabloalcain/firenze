7fd08b25f51d55498e8245820db07432e60d0f4e
Pablo Alcain <pabloalcain@gmail.com>
2023-04-30 14:49:47 -0300

    feat: load one cell notebook

--- tests/test_firenze.py
+++ tests/test_firenze.py
@@ -1,2 +1,16 @@
-def test_everything_is_ok():
-    assert True
+import pathlib
+
+import nbformat
+
+
+class Notebook:
+    def __init__(self, notebook: nbformat.notebooknode.NotebookNode):
+        self.jupyter_notebook = notebook
+
+
+def test_loading_one_cell_notebook():
+    one_cell_notebook_path = pathlib.Path(__file__).parent / "data/one_cell_notebook.ipynb"
+    with open(one_cell_notebook_path) as f:
+        jupyter_notebook = nbformat.read(f, as_version=4)
+    notebook = Notebook(jupyter_notebook)
+    assert notebook.jupyter_notebook == jupyter_notebook
